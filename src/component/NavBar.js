735e64308ec73a485f110e139b09eb763407a6dc
bdupitas <brandondupitas@gmail.com>
2021-06-03 20:29:19 -0700

    added blinking

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,27 +1,38 @@
-import React from "react"
-import Nav from 'react-bootstrap/Nav'
-
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "../css/NavBar.css";
 
 const NavBar = () => {
-    return (
-        <Nav>
-            <Nav.Item>
-                <Nav.Link eventKey="1" href="#/home">
-                    NavLink 1 content
-        </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="2" title="Item">
-                    NavLink 2 content
-        </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="3" disabled>
-                    NavLink 3 content
-        </Nav.Link>
-            </Nav.Item>
-        </Nav>
-    )
-}
+  return (
+    <Navbar>
+      <Navbar.Brand href="#home">
+        <span>
+          {">"} bdupitas&nbsp;
+          <span className="animate-cursor">
+            <span> &nbsp;</span>
+          </span>
+        </span>
+      </Navbar.Brand>
+      <Nav>
+        <Nav.Item>
+          <Nav.Link eventKey="1" href="#/home">
+            NavLink 1 content
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="2" title="Item">
+            NavLink 2 content
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="3" disabled>
+            NavLink 3 content
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
