e0b68484b79dd3fe413d36848f11973d55732c38
bdupitas <brandondupitas@gmail.com>
2021-06-03 21:06:48 -0700

    added links and converted nav bar to reactbootstrap

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,6 +1,7 @@
 import React from "react"
 import NavBar from "./NavBar"
 
+
 const Body = () => {
     return (
         <div className="container">
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,32 +2,33 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "../css/NavBar.css";
+import resume from "../docs/brandon-website-resume.pdf"
 
 const NavBar = () => {
   return (
     <Navbar>
       <Navbar.Brand href="#home">
-        <span>
+        <span className="brand-text">
           {">"} bdupitas&nbsp;
           <span className="animate-cursor">
             <span> &nbsp;</span>
           </span>
         </span>
       </Navbar.Brand>
-      <Nav>
+      <Nav className="justify-content-end ml-auto" >
         <Nav.Item>
-          <Nav.Link eventKey="1" href="#/home">
-            NavLink 1 content
+          <Nav.Link eventKey="1" title="linkedin" target="_blank" href="https://www.linkedin.com/in/bdupitas/" >
+            LinkedIn
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="2" title="Item">
-            NavLink 2 content
+          <Nav.Link eventKey="2" title="github" target="_blank" href= "https://github.com/bdupitas">
+            GitHub
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="3" disabled>
-            NavLink 3 content
+          <Nav.Link eventKey="3" title="resume" target="_blank" href={resume}>
+            Resume
           </Nav.Link>
         </Nav.Item>
       </Nav>
