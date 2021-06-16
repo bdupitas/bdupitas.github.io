1c9f71ac654a22324a6066867f0f161c8c82825d
bdupitas <brandondupitas@gmail.com>
2021-06-15 19:08:44 -0700

    adjusted nav-bar styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Body from "./component/Body"
 
--- src/component/Intro.js
+++ src/component/Intro.js
@@ -4,11 +4,17 @@ import ProfilePhoto from "../docs/bran.png"
 import Col from 'react-bootstrap/Col'
 import "../css/Intro.css"
 
+// profile photo / name and description
+
 const Intro = () => {
   return <>
-  <Col xs={6}>
+  <Col xs={4}>
     <Image className="profile-photo" src={ProfilePhoto} rounded/>
   </Col> 
+  <Col>
+  <h1 className="top-name">Brandon Dupitas</h1>
+  <p>Aspiring Developer based in the Los Angeles Area.</p>
+  </Col>
    </>;
 };
 
